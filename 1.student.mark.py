0ec63f671bbf7cef3ccded2fde5374cbd837be1d
An <An96@example.com>
2024-01-17 13:53:22 +0700

    1.student.mark

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -4,8 +4,7 @@ def input_students():
         (input("Enter student ID: "), 
          input("Enter student name: "), 
          input("Enter student DoB: ")) 
-         for _ in range(num_students)
-         ]
+         for _ in range(num_students)]
 
 def input_courses():
     num_courses = int(input("Enter the number of courses: "))
@@ -13,8 +12,7 @@ def input_courses():
         {'id': input("Enter course ID: "), 
          'name': input("Enter course name: "), 
          'marks': {}} 
-         for _ in range(num_courses)
-         ]
+         for _ in range(num_courses)]
 
 def input_marks(students, courses):
     for course in courses:
@@ -24,17 +22,17 @@ def input_marks(students, courses):
 
 def list_courses(courses):
     print("\nList of Courses:")
-    for course in courses:
-        print(f"{course['id']}: {course['name']}")
+    for i, course in enumerate(courses, start=1):
+        print(f"{i}. {course['id']}: {course['name']}")
 
 def list_students(students):
     print("\nList of Students:")
-    for student in students:
-        print(f"ID: {student[0]} - Name: {student[1]}, DoB: {student[2]}")
+    for i, student in enumerate(students, start=1):
+        print(f"{i}. ID: {student[0]} - Name: {student[1]}, DoB: {student[2]}")
 
 def show_student_marks(students, courses):
-    for course in courses:
-        print(f"\n{course['name']} ({course['id']}) - Student Marks:")
+    for i, course in enumerate(courses, start=1):
+        print(f"\n{i}. {course['name']} ({course['id']}) - Student Marks:")
         for student in students:
             marks = course['marks'].get(student[0], 'N/A (Not graded)')
             print(f"   {student[1]}: {marks}")
